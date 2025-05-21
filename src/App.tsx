2bb1a069a047405340d24ba7db8ed5d08ec51960
Leonid Petrov <lvpjsdev@gmail.com>
2025-05-21 18:00:39 +0300

    feat: add antd ui library and some base components

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,29 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Typography, Input, Checkbox, Flex, Radio, Button } from 'antd';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React!!!!!!</h1>
+      <Typography.Title>todos</Typography.Title>
       <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <Input placeholder='What needs to be done?' />
+        <div>
+          <Checkbox>Hello World</Checkbox>
+          <Checkbox>Hello World</Checkbox>
+          <Checkbox>Hello World</Checkbox>
+        </div>
       </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <footer>
+        <span>counter lefts</span>
+        <Flex gap='middle'>
+          <Radio.Group defaultValue={'all'}>
+            <Radio value='all'>All</Radio>
+            <Radio value='active'>Active</Radio>
+            <Radio value='completed'>Completed</Radio>
+          </Radio.Group>
+        </Flex>
+        <Button type='primary'>Clear completed</Button>
+      </footer>
     </>
   );
 }
