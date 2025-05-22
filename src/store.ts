1633b05eed7b2ab0e3eae5b4cacba31d4b2c3e49
Leonid Petrov <lvpjsdev@gmail.com>
2025-05-22 12:25:43 +0300

    feat: add presist store

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import type { Todo } from './types';
 
 type TodosState = {
@@ -9,33 +10,40 @@ type TodosState = {
   clearDone: () => void;
 };
 
-export const useTodosStore = create<TodosState>()((set, get) => ({
-  todos: [],
-  add: (text: string) => {
-    set({
-      todos: [
-        ...get().todos,
-        {
-          id: (get().todos[get().todos.length - 1]?.id || 0) + 1,
-          text,
-          completed: false,
-        },
-      ],
-    });
+export const useTodosStore = create<TodosState>()(
+  persist(
+    (set, get) => ({
+      todos: [],
+      add: (text: string) => {
+        set({
+          todos: [
+            ...get().todos,
+            {
+              id: (get().todos[get().todos.length - 1]?.id || 0) + 1,
+              text,
+              completed: false,
+            },
+          ],
+        });
 
-    console.log(get().todos);
-  },
-  remove: (id: number) => {
-    set({ todos: get().todos.filter((todo) => todo.id !== id) });
-  },
-  toggle: (id: number) => {
-    set({
-      todos: get().todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      ),
-    });
-  },
-  clearDone: () => {
-    set({ todos: get().todos.filter((todo) => !todo.completed) });
-  },
-}));
+        console.log(get().todos);
+      },
+      remove: (id: number) => {
+        set({ todos: get().todos.filter((todo) => todo.id !== id) });
+      },
+      toggle: (id: number) => {
+        set({
+          todos: get().todos.map((todo) =>
+            todo.id === id ? { ...todo, completed: !todo.completed } : todo
+          ),
+        });
+      },
+      clearDone: () => {
+        set({ todos: get().todos.filter((todo) => !todo.completed) });
+      },
+    }),
+    {
+      name: 'todos-storage',
+    }
+  )
+);
